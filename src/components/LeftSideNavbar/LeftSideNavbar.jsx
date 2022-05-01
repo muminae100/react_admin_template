deb5bbaae0caab6979b113e77fe435f0efce9eb5
foreverkenyan <smuminaetx100@gmail.com>
2022-05-01 21:18:57 +0300

    Initialize react app and add files

---
+++ src/components/LeftSideNavbar/LeftSideNavbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const LeftSideNavbar = () => {
+  return (
+    <div>LeftSideNavbar</div>
+  )
+}
+
+export default LeftSideNavbar;
---
+++ src/components/RightSideChatBar/RightSideChatBar.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const RightSideChatBar = () => {
+  return (
+    <div>RightSideChatBar</div>
+  )
+}
+
+export default RightSideChatBar;
